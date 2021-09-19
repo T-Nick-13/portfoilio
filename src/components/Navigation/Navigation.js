e12b4345d34ae5bb602585dc4a5ff3ed29291839
Nikita Titov <netitov@gmail.com>
2021-09-19 11:05:04 +0300

    create sub meni

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,22 +4,24 @@ import React from 'react';
 function Navigation() {
 
   const [openedMenu, setOpenMenu] = React.useState(false);
+  const [openedSubMenu, SetOpenedSubMenu] = React.useState(false);
 
   const menuClass = openedMenu ? 'menu menu_active' : 'menu';
   const burgerClass = openedMenu ? 'burger burger_inactive' : 'burger';
+  const liClass = openedSubMenu ? '_active' : '';
 
 
   function handleBurgerClick() {
     openedMenu ? setOpenMenu(false) : setOpenMenu(true);
   }
 
-  function handleMenuItemClick() {
-    openedMenu ? setOpenMenu(false) : setOpenMenu(true);
+  function handleArrowClick() {
+    openedSubMenu ? SetOpenedSubMenu(false) : SetOpenedSubMenu(true);
   }
 
+
+
   const currentLocation = useLocation().pathname;
-
-
 
   return (
     <>
@@ -32,10 +34,26 @@ function Navigation() {
           <Link to="/about" className={currentLocation === '/about' ? 'menu__li menu__li_active' : 'menu__li'}
             onClick={handleBurgerClick}>About</Link>
         </li>
-        <li>
+
+
+        <li className={liClass}>
           <Link to="/" className={currentLocation === '/' || currentLocation ==='/works' ? 'menu__li menu__li_active' : 'menu__li'}
             onClick={handleBurgerClick}>Works</Link>
+          <span className="menu__arrow" onClick={handleArrowClick}></span>
+          <ul className="menu__sub-ul">
+            <li >
+              <Link to="#" className="menu__sub-li">Advertising</Link>
+            </li>
+            <li>
+              <Link to="#" className="menu__sub-li">Stickers</Link>
+            </li>
+            <li>
+              <Link to="#" className="menu__sub-li">People</Link>
+            </li>
+          </ul>
         </li>
+
+
         <li>
           <Link to="/contacts" className={currentLocation === '/contacts' ? 'menu__li menu__li_active' : 'menu__li'}
           onClick={handleBurgerClick}>Contacts</Link>
