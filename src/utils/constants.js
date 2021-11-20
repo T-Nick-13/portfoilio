9674071f5edb99bc43110cfd30d8e91e130ecd4e
Nikita Titov <netitov@gmail.com>
2021-11-20 11:28:07 +0300

    gradient for card tag, adding new img

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,102 +1,101 @@
-import img1 from '../images/light/recycle.jpeg';
-import img4 from '../images/light/salefriday.jpg';
-import img2 from '../images/light/vaccine.jpeg';
-import img3 from '../images/light/delivers.jpg';
-import test from '../images/light/12345.jpg';
-import test1 from '../images/light/SET.svg';
-import test2 from '../images/light/AdobeStock_446193555.jpeg';
-import test3 from '../images/light/Открытка2.jpg';
+import recycle from '../images/light/recycle.jpeg';
+import salefriday from '../images/light/salefriday.jpg';
+import vaccine from '../images/light/vaccine.jpeg';
+import delivers from '../images/light/delivers.jpg';
+import ny2021 from '../images/light/12345.jpg';
+import SET2021 from '../images/light/SET.svg';
+import itsgirl from '../images/light/AdobeStock_446193555.jpeg';
+import ny2021b from '../images/light/Открытка2.jpg';
 
-import img5 from '../images/light/bigsale.jpg';
-import img6 from '../images/light/veterans.jpg';
-import img7 from '../images/light/31oct.jpg';
-import img8 from '../images/light/St-min.jpg';
-import img9 from '../images/light/new-min.jpg';
-import img10 from '../images/light/thanks-min.jpg';
-import img11 from '../images/light/againhalloween-min.jpg';
-import img12 from '../images/light/families-min.jpg';
+import bigsale70 from '../images/light/bigsale.jpg';
+import veterans from '../images/light/veterans.jpg';
+import sale31oct from '../images/light/31oct.jpg';
+import peopleGreen from '../images/light/St-min.jpg';
+import vaccineSet from '../images/light/new-min.jpg';
+import ThanksPeople from '../images/light/thanks-min.jpg';
+import hallSale from '../images/light/againhalloween-min.jpg';
+import familyPeople from '../images/light/families-min.jpg';
 
 
 export const pic = [
   {
-    link: img9,
-    name: 'testy',
-    tag: '#test'
+    link: vaccineSet,
+    name: 'vaccineSet',
+    tag: '#sticker'
   },
   {
-    link: img10,
-    name: 'recycle',
+    link: ThanksPeople,
+    name: 'ThanksPeople',
+    tag: '#people'
+  },
+  {
+    link: hallSale,
+    name: 'hallSale',
     tag: '#advertising'
   },
   {
-    link: img11,
+    link: delivers,
+    name: 'delivers',
+    tag: '#card'
+  },
+  {
+    link: vaccine,
     name: 'vaccine',
     tag: '#sticker'
   },
   {
-    link: img3,
-    name: 'delivers1',
+    link: itsgirl,
+    name: 'itsgirl',
+    tag: '#card'
+  },
+  {
+    link: ny2021,
+    name: 'ny2021',
+    tag: '#holidays'
+  },
+  {
+    link: recycle,
+    name: 'recycle',
     tag: '#sticker'
   },
   {
-    link: test1,
-    name: 'delivers2',
-    tag: '#test2'
+    link: SET2021,
+    name: 'SET2021',
+    tag: '#holidays'
   },
   {
-    link: test2,
-    name: 'delivers3',
-    tag: '#advertising'
-  },
-  {
-    link: img3,
-    name: 'delivers4',
+    link: salefriday,
+    name: 'salefriday',
     tag: '#advertising'
   },
   {
-    link: img1,
-    name: 'delivers5',
-    tag: '#test4'
+    link: peopleGreen,
+    name: 'peopleGreen',
+    tag: '#people'
   },
   {
-    link: test,
-    name: 'delivers6',
+    link: bigsale70,
+    name: 'bigsale70',
     tag: '#advertising'
   },
   {
-    link: img4,
-    name: 'delivers6',
-    tag: '#advertising'
-  }
-  ,
+    link: veterans,
+    name: 'veterans',
+    tag: '#holidays'
+  },
   {
-    link: img8,
-    name: 'delivers6',
+    link: sale31oct,
+    name: 'sale31oct',
     tag: '#advertising'
-  }
-  ,
+  },
   {
-    link: img5,
-    name: 'delivers6',
-    tag: '#advertising'
-  }
-  ,
+    link: ny2021b,
+    name: 'ny2021b',
+    tag: '#holidays'
+  },
   {
-    link: img6,
-    name: 'delivers6',
-    tag: '#advertising'
+    link: familyPeople,
+    name: 'familyPeople',
+    tag: '#people'
   }
-  ,
-  {
-    link: img7,
-    name: 'delivers6',
-    tag: '#advertising'
-  }
-  ,
-  {
-    link: img11,
-    name: 'delivers6',
-    tag: '#advertising'
-  }
-
 ];
