c3b0d1cb76b21e15aeadfae48999a738612f063e
Nikita Titov <netitov@gmail.com>
2023-01-04 19:33:06 +0300

    front: add picture in main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import Card from '../Card/Card';
+import Promo from '../Promo/Promo';
 
 
 function Main(props) {
@@ -13,18 +14,21 @@ function Main(props) {
 
   return (
     <main className="main">
-      {props.pic.map((card) =>{
-        return (
-          <Card
-            card={card}
-            tag={card.tag}
-            key={card._id}
-            onCardClick={props.handleCardClick}
-            onTagClick={props.handleTagClick}
-            setClickedTag={props.setClickedTag}
-          />
-        )
-      })}
+      <Promo />
+      <section className="main__pictures">
+        {props.pic.map((card) =>{
+          return (
+            <Card
+              card={card}
+              tag={card.tag}
+              key={card._id}
+              onCardClick={props.handleCardClick}
+              onTagClick={props.handleTagClick}
+              setClickedTag={props.setClickedTag}
+            />
+          )
+        })}
+      </section>
       <p className={`filter__btn ${menuBtnClass}`} onClick={handleFilterDropping}>Show all works</p>
     </main>
 
