d15065412ed09aa238b631a350211533f0783cd4
Nikita Titov <netitov@gmail.com>
2023-01-14 22:56:22 +0300

    front: add promo, and update comp about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,39 +1,29 @@
 import React from 'react';
 
-import logoPath from '../../images/Video.svg';
+import me from '../../images/light/Me.svg';
 
 function About() {
 
   return (
     <section className="about">
-
-      <div className="about__container">
-        <img className="about__photo" alt="photo" src={logoPath}></img>
-        <div className="about__info">
-          <h1 className="about__name">Natalya Stafeeva</h1>
-          <p className="about__location">Saint-Petersburg, Russia</p>
-          <p className="about__specialization">Illustration</p>
-        </div>
-      </div>
-
-      <div className="about__container2">
-
-      <div className="about__general">
-        <h1 className="about__header">About</h1>
-        <p className="about__paragraph">Hello! I'm an Illustrater with 2 years
-        background and more than 3 hundreds vectors sold. My works are applied
-        in advertisement, site, presentation and so on</p>
-      </div>
-
-      <div className="about__contacts">
-        <h1 className="about__header">You can contact me</h1>
-        <p className="about__paragraph"><span className="about__contact-type">email:</span> natalya-stafeeva@mail.ru</p>
-        <p className="about__paragraph"><span className="about__contact-type">instagram: </span>
-          <a className="about__link link" href="https://www.instagram.com/natalyastafeeva" target="_blank">natalyastafeeva</a>
+      <div className="about__block promo__block">
+        <img className="about__img promo__img" src={me} alt="workplace"></img>
+        <p className="about__info">
+          Hello!<br></br>
+          My name is Natalia.<br></br>
+          I'm a vector Illustrater with<br></br>
+          <b>3 years</b> background and more than<br></br>
+          <b>3 000 Illustrations</b> sold.<br></br>
+          My works are applied in<br></br>
+          advertisement, websites,<br></br>
+          presentation and so on:)
         </p>
       </div>
+      <p className="about__afterwords">
+        If you need beautiful modern<br></br>
+        vector design, just let me know:)
+      </p>
 
-      </div>
 
     </section>
   );
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import promoPath from '../../images/light/Безымянный-6.svg';
-import mouse from '../../images/light/Mouse.svg';
+import mouse from '../../images/light/Pero.svg';
 
 
 function Promo(props) {
@@ -8,17 +8,17 @@ function Promo(props) {
   return (
     <section className="promo">
       <div className="promo__block">
-      <div className="promo__container">
-        <div className="promo__info">
-          <h1 className="promo__info-text promo__h1">Vector Illustrations</h1>
-          <h2 className="promo__info-text promo__h2">MODERN MINIMAL DESIGN</h2>
-        </div>
-        <div className="promo__img-container">
-          <img className="promo__img" src={promoPath} alt="promo"></img>
-          <img className="promo__img-mouse" src={mouse} alt="mouse"></img>
-        </div>
+        <div className="promo__container">
+          <div className="promo__info">
+            <h1 className="promo__info-text promo__h1">Vector Illustrations</h1>
+            <h2 className="promo__info-text promo__h2">MODERN MINIMAL DESIGN</h2>
+          </div>
+          <div className="promo__img-container">
+            <img className="promo__img" src={promoPath} alt="promo"></img>
+            <img className="promo__img-mouse" src={mouse} alt="mouse"></img>
+          </div>
 
-      </div>
+        </div>
       </div>
     </section>
   );
