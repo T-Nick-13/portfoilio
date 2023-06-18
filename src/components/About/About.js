f0e6fc02556e7035169eed9e27cc118d9ae97770
Nikita Titov <netitov@gmail.com>
2023-06-18 21:43:19 +0200

    interface: adaptive layout for page About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,25 +2,26 @@ import React from 'react';
 import Contacts from '../Contacts/Contacts';
 
 import aboutImg01 from '../../images/light/aboutImg01.png';
+import aboutImg01_s from '../../images/light/aboutImg01_small.png';
 import aboutImg02 from '../../images/light/aboutImg02.png';
 import aboutPhoto01 from '../../images/light/about1.jpg';
 import aboutPhoto02 from '../../images/light/about2.jpg';
 import aboutPhoto03 from '../../images/light/about3.jpg';
 
-function About(props) {
+function About() {
 
   return (
     <div className="about">
-
       <div className="dark-box about__block">
         <div className="dark-box__img-cont">
           <p className='dark-box__text about__text'>
-            My name is Stafeeva Natalia<br/>
+            My name is Stafeeva Natalia <br/>
             I'm an Illustrator with<br/>
             3 years background<br/>
           </p>
           <img className="dark-box__back-img dark-box__back-img_left" src={aboutImg01} alt="workplace"></img>
         </div>
+        <img className="dark-box__back-img dark-box__back-img_small" src={aboutImg01_s} alt="workplace"></img>
         <div className="dark-box__overlay"></div>
       </div>
 
@@ -43,17 +44,18 @@ function About(props) {
       </div>
 
       <div className="dark-box dark-box_small about__block">
-        <div className="dark-box__img-cont">
-          <p className="dark-box__text about__text">
+        <div className="dark-box__img-cont dark-box__img-cont_small">
+          <div>
+            <img className="dark-box__photo dark-box__photo_main" src={aboutPhoto02} alt="my photo"></img>
+            <img className="dark-box__photo dark-box__photo_back" src={aboutPhoto03} alt="my photo"></img>
+          </div>
+          <p className="dark-box__text dark-box__text_small about__text">
             In my works I<br/>
             prefer simplicity <br/>
             modernity and minimalism<br/>
           </p>
-          <img className="dark-box__photo dark-box__photo_main" src={aboutPhoto02} alt="my photo"></img>
-          <img className="dark-box__photo dark-box__photo_back" src={aboutPhoto03} alt="my photo"></img>
-          <img className="dark-box__back-img dark-box__back-img_right" src={aboutImg02} alt="workplace"></img>
-
         </div>
+        <img className="dark-box__back-img dark-box__back-img_right" src={aboutImg02} alt="workplace"></img>
         <div className="dark-box__overlay"></div>
       </div>
 
